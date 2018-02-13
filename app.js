475a2cf198e8f9db65337be9d14d9193077a38c4
Md-Hiccup <ask2md@gmail.com>
2018-02-13 22:26:12 +0530

    added CRUD operation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-mongoose.connect('mongodb://localhost/white-panda', { useMongoClient: true, promiseLibrary: require('bluebird')})
+mongoose.connect('mongodb://localhost/mean-book', { useMongoClient: true, promiseLibrary: require('bluebird')})
     .then(() => console.log('connection successful'))
     .catch((err) => console.error(err));
 
--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Book = require('../models/Book');
 
-/* GET home page. */
+/* GET All Books */
 router.get('/', function(req, res, next) {
-  res.send('Express RESTful API');
+  // res.json('Show the all Book')
+  Book.find(function (err, products){
+    if(err) return next(err);
+    res.json(products);
+  })
 });
+
+/* GET Single Book By Id */
+router.get('/:id', function(req, res, next){
+  Book.findById(req.params.id, function(err, post){
+    if(err) return next(err); 
+    res.json(post)
+  })
+})
+
+/* SAVE Book */
+router.post('/', function(req, res, next){
+  Book.create(req.body, function(err, post){
+    if(err) return next(err);
+    res.json(post);
+  })
+})
+
+/* UPDATE Book */
+router.put('/:id', function(req, res, next){
+  Book.findById(req.params.id, function(err, post){
+    if(err) return next(err);
+    res.json(post);
+  })
+})
+
+/* DELETE Book */
+router.delete('/', function(req, res, next){
+  Book.findByIdAndRemove(req.params.id, function(err, post){
+    if(err) return next(err);
+    res.json(post)
+  })
+})
+
 
 module.exports = router;
